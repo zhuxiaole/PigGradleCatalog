02d56bcaf8f477ecff4a93b5591d51a1d831d924
猪小乐 <zhuxiaole@zhuxiaole.org>
2023-02-08 21:49:16 +0800

    v0.0.1版本

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.application)
+    alias(libs.plugins.kotlin.android)
+    `version-catalog`
+    `maven-publish`
+}
+
+android {
+    namespace = "org.zhuxiaole.gradle.catalog"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "org.zhuxiaole.gradle.catalog"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+catalog {
+    versionCatalog {
+        from(files("../libs.versions.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.zhuxiaole.gradle"
+            artifactId = "catalog"
+            version = "0.0.1"
+            from(components["versionCatalog"])
+        }
+    }
+}
