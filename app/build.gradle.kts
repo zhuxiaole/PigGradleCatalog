18a7c7cbe9ad7c16bd4f9977e6008e5fe216ac21
猪小乐 <zhuxiaole@zhuxiaole.org>
2023-02-08 23:01:10 +0800

    修改sdk版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "org.zhuxiaole.gradle.catalog"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "org.zhuxiaole.gradle.catalog"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
